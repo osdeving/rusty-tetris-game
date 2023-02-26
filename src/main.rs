cf2bf09fab283ecb8b0697c0b3721a5dda82ed4a
Willams Sousa <osdeving@gmail.com>
2023-02-26 00:28:25 -0300

    feat: :sparkles: create a window using sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::{time::Duration, thread::sleep};
+
+use sdl2::{pixels::Color, event::Event, keyboard::Keycode};
+
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().expect("SDL Initialization Failed.");
+
+    let video_subsystem = sdl_context
+        .video()
+        .expect("Couldn't get SDL Video Subsystem");
+
+    let window = video_subsystem
+        .window("Rust Tetris Game", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .expect("Failed to create window");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to convert window into canvas.");
+
+    canvas.set_draw_color(Color::RGB(255,120,255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_bump = sdl_context.event_pump().expect("Failed to get SDL event bump");
+
+    'running: loop {
+        for event in event_bump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } =>
+                {
+                    break 'running
+                },
+                _ => {}
+            }
+            sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+    }
+
 }
